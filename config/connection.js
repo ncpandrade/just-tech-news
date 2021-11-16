ebf63f420777b1916c4095e1195e9bb97a4da53e
Nadine <nadine_paez@hotmail.com>
2021-11-16 11:06:25 -0500

    finished routes

--- config/connection.js
+++ config/connection.js
@@ -3,10 +3,14 @@ const Sequelize = require('sequelize');
 require("dotenv").config();
 
 // create connection to our database, pass in your MySQL information for username and password
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.USER, process.env.PASS, {
   host: 'localhost',
   dialect: 'mysql',
   port: 3306
 });
+}
 
 module.exports = sequelize;
